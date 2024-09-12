ef62972dedd37e90ece183042d437af6b08a90b1
EronSalicano <161715505+EronSalicano@users.noreply.github.com>
2024-09-12 08:36:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log( "meu primeiro progrma: trabalhando com variavens")
+const idade=26
+console.log(idade+2)
+console.log(idade-2)
+console.log(idade/2)
+console.log(idade*2)
+
+
+
+const somaIdade=idade+2
